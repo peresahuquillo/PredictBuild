7e755a316d536b59abc398d617235224de452fc6
Pere Sahuquillo <150919883+peresahuquillo@users.noreply.github.com>
2026-03-02 20:41:40 +0100

    S'ha afegit el schema a les taules

--- main.py
+++ main.py
@@ -61,18 +61,18 @@ def cargar_diccionarios():
     tipos_vivienda = dict(zip(tipos_df['tipo_vivienda'], tipos_df['index']))
     
     # Diccionario de estado de obra
-    estados_df = pd.read_sql('SELECT estado, valor FROM "LU_Estat_Immoble"', conn)
+    estados_df = pd.read_sql('SELECT estado, valor FROM core."LU_Estat_Immoble"', conn)
     estados_obra = dict(zip(estados_df['estado'], estados_df['valor']))
     
     # Tabla principal con precios
     precios_df = pd.read_sql('''
         SELECT ciudad, distrito, barrio, calle, 
                precio_m2_distrito, precio_m2_barrio, precio_m2_calle
-        FROM "LU_Preu_m2_Districte_Barri_Carrer"
+        FROM core."LU_Preu_m2_Districte_Barri_Carrer"
     ''', conn)
     
     # Mapeo distrito -> modelo y distrito -> grupo (distrito_valor)
-    mapeo_df = pd.read_sql('SELECT grupo, ciudad, distrito, modelo FROM "LU_Modelos_Distrito"', conn)
+    mapeo_df = pd.read_sql('SELECT grupo, ciudad, distrito, modelo FROM core."LU_Modelos_Distrito"', conn)
     mapeo_modelos = dict(zip(mapeo_df['distrito'], mapeo_df['modelo']))
     distrito_valores = dict(zip(mapeo_df['distrito'], mapeo_df['grupo']))
     
